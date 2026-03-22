87ddcb14c7f19f59b0ac8e92e06766b09886867e
Kirill <kirillepiskin9@gmail.com>
2026-03-22 13:08:50 +0300

    task6 and update task4

--- web2.4.py
+++ web2.4.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-answer = {'surname': 'Дуров', 'name': 'Павел', 'education': 'Филологическое', 'profession': 'Штурман марсохода',
-          'sex': 'male', 'motivation': 'Серьезная', 'ready': 'True'}
+answer = {'Surname': 'Дуров', 'Name': 'Павел', 'Education': 'Филологическое', 'Profession': 'Штурман марсохода',
+          'Sex': 'male', 'Motivation': 'Серьезная', 'Ready': 'True'}
 
 @app.route('/')
 @app.route('/index')
@@ -11,9 +11,12 @@ def main_str():
     title = "Колонизация Марса"
     return render_template('base3.html', title=title)
 
+@app.route('/answer')
+def answ():
+    return render_template('base6.html', answer=answer)
 
 @app.route('/auto_answer')
-def hello():
+def auto_answ():
     return render_template('base4.html', answer=answer)
 
 
---
+++ web2.5.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+distribution = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
+
+
+@app.route('/distribution')
+def distrib():
+    return render_template('base5.html', distr=distribution)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host='127.0.0.1')
